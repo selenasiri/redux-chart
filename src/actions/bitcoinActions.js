d0d18238a7f75819d4806a48e6b0b51504685c10
Selena Siri <selenasiri@gmail.com>
2020-11-08 14:14:13 -0500

    added key

--- src/actions/bitcoinActions.js
+++ src/actions/bitcoinActions.js
@@ -7,13 +7,24 @@ export const getData = ({ time, number }) => async dispatch => {
       type: "AWAITING_BITCOIN"
     })
 
-    const response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-chart/${time}/BTCUSD`)
+   // https://financialmodelingprep.com/api/v3/historical-chart/1min/BTCUSD?apikey=deme
+    /*
+    [ {
+      "date" : "2020-11-08 13:45:00",
+      "open" : 15533.268000000000,
+      "low" : 15497.087000000000,
+      "high" : 15533.268000000000,
+      "close" : 15497.087000000000,
+      "volume" : 30837102592
+    }*/
+
+    const response = await axios.get(`https://financialmodelingprep.com/api/v3/historical-chart/${time}/BTCUSD?apikey=${process.env.REACT_APP_API_KEY}`)
 
     const labels = [];
     const data = [];
     for (let i = 0; i < response.data.length; i++) {
       data.unshift(response.data[i].close)
-      labels.unshift(moment(response.data[i].date).format("LT"))
+      labels.unshift(moment(response.data[i].date).format("LT")) // only getting close and date
 
       if (i === (number - 1)) {
         break;
